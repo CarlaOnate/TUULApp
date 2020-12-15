820d30e3d3a638b1df86bd76f508f90e62445281
CarlaOnate <carlaonategardella@gmail.com>
2020-12-15 13:01:26 -0600

    Separated google signin button in diff component

---
+++ frontend/src/components/auth/Google-singin.js
@@ -0,0 +1,99 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useState, useEffect} from 'react';
+import {
+    View,
+    Text,
+} from 'react-native';
+import styles from '../../styles/social-signin.styles'
+
+
+
+import { GoogleSignin, GoogleSigninButton, statusCodes } from 'react-native-google-signin';
+
+const GoogleSigninComponent = ({navigation, errorState, context, loginUser, loginUserResponse}) => {
+    const {data, userError} = loginUserResponse
+    const {error, setError} = errorState
+
+    const [googleLoading, setGoogleLoading] = useState(false)
+
+    GoogleSignin.configure();
+
+    const loginResponse = () => {
+        console.log('loginRes', data)
+        if(data){
+            console.log('user', data.loginUser)
+            context.setUser(data.loginUser)
+            navigation.navigate('Dashboard')
+        }
+    }
+
+    useEffect(loginResponse ,[data, userError])
+
+    const signInGoogle = async () => {
+        try {
+            error && setError(userError.message)
+            setGoogleLoading(false)
+
+            await GoogleSignin.hasPlayServices();
+            const {idToken, user: {email, id: googleId, photo, name}} = await GoogleSignin.signIn();
+
+            console.log(email, name)
+
+            await loginUser({variables: {
+                    input: {
+                        type: 'google',
+                        idToken,
+                        googleAccount: {
+                            email,
+                            name,
+                            photo,
+                            googleId
+                        }
+                    }
+                }})
+
+        } catch (error) {
+            console.log(error)
+            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+                // user cancelled the login flow
+                setGoogleLoading(false)
+            } else if (error.code === statusCodes.IN_PROGRESS) {
+                // operation (f.e. sign in) is in progress already
+                setGoogleLoading(true)
+            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+                // play services not available or outdated
+                setError('Google Play Services no están disponibles')
+                setGoogleLoading(false)
+            } else {
+                // some other error happened
+                setError('Hubo un error')
+                setGoogleLoading(false)
+            }
+        }
+    };
+
+
+    return (
+        <>
+        <View style={styles.sectionContainer}>
+            <Text style={styles.sectionTitle}>Loggea con Google</Text>
+            <GoogleSigninButton
+                style={{ width: 192, height: 48 }}
+                size={GoogleSigninButton.Size.Wide}
+                color={GoogleSigninButton.Color.Dark}
+                onPress={signInGoogle}
+                disabled={googleLoading} />
+        </View>
+        </>
+    );
+};
+
+
+export default GoogleSigninComponent;
--- frontend/src/pages/Social-signin.js
+++ frontend/src/pages/Social-signin.js
@@ -6,28 +6,21 @@
  * @flow strict-local
  */
 
-import React, {useState, useContext, useEffect} from 'react';
+import React, {useState, useContext} from 'react';
 import {
     SafeAreaView,
-    StyleSheet,
     ScrollView,
     View,
-    Text,
     StatusBar,
-    Button
 } from 'react-native';
 
-import {
-    Header,
-    Colors,
-} from 'react-native/Libraries/NewAppScreen';
+import { Header,} from 'react-native/Libraries/NewAppScreen';
+import styles from '../styles/social-signin.styles'
 
-
-import { GoogleSignin, GoogleSigninButton, statusCodes } from 'react-native-google-signin';
 import userContext from '../contexts/userContext'
 import { gql, useMutation } from '@apollo/client';
-import { setContext } from "apollo-link-context";
 
+import GoogleSigninComponent from "../components/auth/Google-singin";
 
 //create user mutation
 const LOGIN_USER = gql`
@@ -44,67 +37,9 @@ const LOGIN_USER = gql`
 
 const SocialSignIn = ({navigation}) => {
     const [error, setError] = useState()
-    const [googleLoading, setGoogleLoading] = useState(false)
-
 
     const context = useContext(userContext)
     const [loginUser, {data, error: userError}] = useMutation(LOGIN_USER)
-
-    GoogleSignin.configure();
-
-    const loginResponse = () => {
-        console.log('loginRes', data)
-        if(data){
-            console.log('user', data.loginUser)
-            context.setUser(data.loginUser)
-            navigation.navigate('Dashboard')
-        }
-    }
-
-    useEffect(loginResponse ,[data, userError])
-
-    const signInGoogle = async () => {
-        try {
-            error && setError(userError.message)
-            setGoogleLoading(false)
-
-            await GoogleSignin.hasPlayServices();
-            const {idToken, user: {email, id: googleId, photo, name}} = await GoogleSignin.signIn();
-
-            console.log(email, name)
-
-            await loginUser({variables: {
-                    input: {
-                        type: 'google',
-                        idToken,
-                        googleAccount: {
-                            email,
-                            name,
-                            photo,
-                            googleId
-                        }
-                    }
-            }})
-
-        } catch (error) {
-            console.log(error)
-            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-                // user cancelled the login flow
-                setGoogleLoading(false)
-            } else if (error.code === statusCodes.IN_PROGRESS) {
-                // operation (f.e. sign in) is in progress already
-                setGoogleLoading(true)
-            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-                // play services not available or outdated
-                setError('Google Play Services no están disponibles')
-                setGoogleLoading(false)
-            } else {
-                // some other error happened
-                setError('Hubo un error')
-                setGoogleLoading(false)
-            }
-        }
-    };
 
 
     return (
@@ -116,21 +51,13 @@ const SocialSignIn = ({navigation}) => {
                     style={styles.scrollView}>
                     <Header />
                     <View style={styles.body}>
-                        <View style={styles.sectionContainer}>
-                            <Text style={styles.sectionTitle}>Loggea con Google</Text>
-                            <GoogleSigninButton
-                                style={{ width: 192, height: 48 }}
-                                size={GoogleSigninButton.Size.Wide}
-                                color={GoogleSigninButton.Color.Dark}
-                                onPress={signInGoogle}
-                                disabled={googleLoading} />
-                        </View>
-                        {error && (<Text>{error}</Text>)}
+                        <GoogleSigninComponent
+                            navigation={navigation}
+                            errorState={{error, setError}}
+                            context={context}
+                            loginUser={loginUser}
+                            loginUserResponse={{data, userError}} />
                     </View>
-                    <Button
-                        title='Dashboard'
-                        onPress={() => navigation.navigate('Dashboard')}
-                    />
                 </ScrollView>
             </SafeAreaView>
         </>
