594370fe4a55b82ee3516630957517944756a088
CarlaOnate <carlaonategardella@gmail.com>
2020-12-19 11:49:51 -0600

    Returned project to commit 550c566709c370054cf70268bcc78e01138f3fd5, before installing maps

--- frontend/src/atoms/Modals/Map.js
+++ frontend/src/atoms/Modals/Map.js
@@ -2,20 +2,11 @@ import React from 'react';
 import {
     View
 } from 'react-native'
-import MapView from 'react-native-maps';
-
 
 const Map = () => {
     return (
         <View>
-            <MapView
-                initialRegion={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                }}
-            />
+
         </View>
     );
 }
