8a3dc853607d9be9b5ca984e11d1c63fcec28100
CarlaOnate <carlaonategardella@gmail.com>
2020-12-21 09:09:13 -0600

    Instaled mapbox succesfully

--- frontend/src/atoms/Modals/Map.js
+++ frontend/src/atoms/Modals/Map.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {
     View
 } from 'react-native'
+import MapboxGL from "@react-native-mapbox-gl/maps";
+
+MapboxGL.setAccessToken("<YOUR_ACCESSTOKEN>");
+MapboxGL.setConnected(true);
+
+useEffect(() => {
+    MapboxGL.setTelemetryEnabled(false);
+}, [])
+
 
 const Map = () => {
     return (
-        <View>
-
+        <View style={styles.page}>
+            <View style={styles.container}>
+                <MapboxGL.MapView style={styles.map} />
+            </View>
         </View>
     );
 }
